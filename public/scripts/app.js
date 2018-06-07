bc638b5db686aeb74978cd7ef232feedd1c9750c
Jagadish <jagadish@moogilu.com>
2018-06-07 07:50:05 +0100

    using Object with JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -31,6 +31,58 @@ var template = React.createElement(
     )
   )
 );
+
+var user = {
+  name: 'Jagadish',
+  age: 49,
+  location: 'Mountain House'
+};
+
+var userName = 'Jagadish';
+var userAge = 49;
+var userLocation = 'Mountain House';
+
+var templateNoObject = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    userName
+  ),
+  React.createElement(
+    'p',
+    null,
+    userAge
+  ),
+  React.createElement(
+    'p',
+    null,
+    userLocation
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    user.location
+  )
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-console.log('App.js is running!');
+console.log('App.js is running!')
 
 // JSX - JavaScript XML
 var template = (
@@ -10,7 +10,33 @@ var template = (
       <li>Item two</li>
     </ol>
   </div>
-);
-var appRoot = document.getElementById('app');
+)
 
-ReactDOM.render(template, appRoot);
+var user = {
+  name: 'Jagadish',
+  age: 49,
+  location: 'Mountain House'
+}
+
+var userName = 'Jagadish'
+var userAge = 49
+var userLocation = 'Mountain House'
+
+var templateNoObject = (
+  <div>
+    <h1>{userName}</h1>
+    <p>{userAge}</p>
+    <p>{userLocation}</p>
+  </div>
+)
+
+var templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+)
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(templateTwo, appRoot)
